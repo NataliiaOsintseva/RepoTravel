9ba7e79876693270e0ad4bba22ccc6f900a18823
NataliiaOsintseva <nataly.osya@gmail.com>
2016-04-25 17:51:06 +0300

    After MVC Completed

--- src/Travel/Controllers/Web/AppController.cs
+++ src/Travel/Controllers/Web/AppController.cs
@@ -4,11 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Travel;
+using Travel.Services;
+using Travel.ViewModels;
 
 namespace Travel.Controllers.Web
 {
     public class AppController : Controller
     {
+        private IMailService _mailService;
+
+        public AppController(IMailService service)
+        {
+            _mailService = service;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -21,6 +31,32 @@ namespace Travel.Controllers.Web
 
         public IActionResult Contact()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var email = Startup.Configuration["AppSettings:SiteEmailAddress"];
+
+                if (string.IsNullOrWhiteSpace(email))
+                {
+                    ModelState.AddModelError("", "Could not send email, configuration problem.");
+                }
+
+                if (_mailService.SendMail(email,
+                  email,
+                  $"Contact Page from {model.Name} ({model.Email})",
+                  model.Message))
+                {
+                    ModelState.Clear();
+
+                    ViewBag.Message = "Mail Sent. Thanks!";
+                }               
+            }
+
             return View();
         }
     }
