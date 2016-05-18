965b42ba0ab232e71dcc18e28a82e8412a2fd4f6
NataliiaOsintseva <nataly.osya@gmail.com>
2016-05-18 18:34:08 +0300

    After Angular

--- src/Travel/wwwroot/js/tripEditorController.js
+++ src/Travel/wwwroot/js/tripEditorController.js
@@ -16,7 +16,9 @@
         vm.isBusy = true;
         vm.newStop = {};
         
-        $http.get("/api/trips/" + vm.tripName + "/stops")
+        var url = "/api/trips/" + vm.tripName + "/stops";
+
+        $http.get(url)
         .then(function (response) {
             // success
             angular.copy(response.data, vm.stops);
@@ -29,6 +31,26 @@
         .finally(function () {
             vm.isBusy = false;
         });
+
+        vm.addStop = function () {
+
+            vm.isBusy = true;
+
+            $http.post(url, vm.newStop)
+            .then(function (response) {
+                // SUCCESS
+                vm.stops.push(response.data);
+                _showMap(vm.stops);
+                vm.newStop = {};
+            }, function (err) {
+                // FAILURE
+                vm.errorMessage = "Failed to add new stop";
+            })
+            .finally(function () {
+                vm.isBusy = false;
+            });
+
+        };
     }
 
     function _showMap(stops) {
